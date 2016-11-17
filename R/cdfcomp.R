b3f5d9ac2f4dd23a4ed8e2495abb93a3dd88ddd6
Marie-Laure Delignette-Muller <marielaure.delignettemuller@vetagro-sup.fr>
2016-11-17 09:55:30 +0000

    Minor modifications on graphcomp.Rd and add of Aurelie on copyright on denscomp.R, ppcomp.R, cdfcomp.R, qqcomp.R

--- R/cdfcomp.R
+++ R/cdfcomp.R
@@ -1,5 +1,5 @@
 #############################################################################
-#   Copyright (c) 2011 Marie Laure Delignette-Muller, Christophe Dutang
+#   Copyright (c) 2011 Marie Laure Delignette-Muller, Christophe Dutang, Aurélie Siberchicot
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
