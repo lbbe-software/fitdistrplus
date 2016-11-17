5e5b56471a0f33a3ce33dda3665a47b61eb7f0fd
Marie-Laure Delignette-Muller <marielaure.delignettemuller@vetagro-sup.fr>
2016-11-17 10:06:28 +0000

    Just to remove the accent in the name of Aurelie in my previous commit

--- R/ppcomp.R
+++ R/ppcomp.R
@@ -1,5 +1,5 @@
 #############################################################################
-#   Copyright (c) 2012 Christophe Dutang, Aurélie Siberchicot
+#   Copyright (c) 2012 Christophe Dutang, Aurelie Siberchicot
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
