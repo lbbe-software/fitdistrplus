a9c4c0d1614a668d178aba39e5bcfc33a81ce12d
Marie-Laure Delignette-Muller <marielaure.delignettemuller@vetagro-sup.fr>
2018-06-15 09:22:11 +0000

    add of two examples for mledist-cens, one with the use of weights and one with start or fix.arg defined as a function of pseudo data

--- tests/t-mledist-cens.R
+++ tests/t-mledist-cens.R
@@ -3,7 +3,7 @@ library(fitdistrplus)
 
 
 
-# (1) fit a continuous distribution (Gumbel) to censored data.
+# (1) fit a user-defined continuous distribution (Gumbel) to censored data.
 #
 
 data(fluazinam)
@@ -31,3 +31,29 @@ mledist(log10EC50, "lnorm", optim.method="BFGS", lower=0) #a simple warning
 #constrOptim() is used
 mledist(log10EC50, "lnorm", optim.method="Nelder", lower=0)
 mledist(log10EC50, "lnorm", lower=0) #error
+
+# (3) weighted MLE
+#
+xleft <- c(-1.8, -0.6, -0.1, 0.07, 0.14, 1, 1.2, 1.2, 1.2)
+xright <- c(-1.8, -0.6, -0.1, 0.07, 0.14, 1, NA, NA, NA)
+d <- data.frame(left = xleft, right = xright)
+f1 <- mledist(d, "norm")
+
+dbis <- data.frame(left = c(-1.8, -0.6, -0.1, 0.07, 0.14, 1, 1.2), 
+                   right = c(-1.8, -0.6, -0.1, 0.07, 0.14, 1, NA))
+f2 <- mledist(dbis, "norm", weights = c(rep(1,6),3))
+
+# f1 and f2 must give quite the same results (only starting values differ)
+f1$estimate
+f2$estimate
+
+# (4) test the definition of fix.arg/start.arg as functions
+#     if defined as functions, start.arg and fix.arg must be
+#     functions of pseudo data (output pseudo of cens2pseudo())
+
+mledist(d, "norm", start = function(x) list(mean = 0, sd = 1))$estimate
+mledist(d, "norm", start = function(x) list(mean = mean(x), sd = 1))$estimate
+mledist(d, "norm", fix.arg = function(x) list(mean = 0))$estimate
+mledist(d, "norm", fix.arg = function(x) list(mean = 0.544))$estimate
+mledist(d, "norm", fix.arg = function(x) list(mean = mean(x)))$estimate
+
