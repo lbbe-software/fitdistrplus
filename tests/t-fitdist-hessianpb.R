d33dc7ca2fad83f8410c4b3aeaee4f4ec97a9f12
Christophe Dutang <dutangc@gmail.com>
2024-05-23 16:07:29 +0200

    Update t-fitdist-hessianpb.R

--- tests/t-fitdist-hessianpb.R
+++ tests/t-fitdist-hessianpb.R
@@ -25,25 +25,26 @@ vcov(fitdist(serving, "gamma", start=as.list(f1$estimate)))
 
 
 #an example with lower bounds for ztbinom
-
-library(actuar)
-n <- 1e3
-x <- rztbinom(n, 30, 1/2)
-lnl(c(35, 1/2), obs=x, dfun=dztbinom)
-
-optim(c(35, 1/2), lnl, obs=x, dfun=dztbinom, control=list(fnscale=-1))
-
-optimHess(c(35, 1/2), lnl, obs=x, dfun=dztbinom)
-
-mledist(x, "ztbinom", fix.arg = list(size=30))
-fitdist(x, "ztbinom", fix.arg = list(size=30))
-
-mledist(x, "ztbinom", lower=0)
-fitdist(x, "ztbinom", lower=0)
-
-dztbinom(5, 30.5, 1/2)
-
-llsurface(data =x, distr="ztbinom", plot.arg=c("size", "prob"), 
-          min.arg=c(max(x), 0.4), max.arg=c(31, 0.6), back.col = FALSE,
-          main="log-likelihood for ZT binomial distribution")
-
+if(FALSE)
+{
+  library(actuar)
+  n <- 1e3
+  x <- rztbinom(n, 30, 1/2)
+  lnl(c(35, 1/2), obs=x, dfun=dztbinom)
+  
+  optim(c(35, 1/2), lnl, obs=x, dfun=dztbinom, control=list(fnscale=-1))
+  optimHess(c(35, ), lnl, obs=x, dfun=dztbinom)
+  
+  mledist(x, "ztbinom", fix.arg = list(size=30))
+  fitdist(x, "ztbinom", fix.arg = list(size=30))
+  
+  mledist(x, "ztbinom", lower=0)
+  fitdist(x, "ztbinom", lower=0)
+  
+  dztbinom(5, 30.5, 1/2)
+  
+  llsurface(data = x, distr="ztbinom", plot.arg=c("size", "prob"), 
+            min.arg=c(max(x)+1, 0.4), max.arg=c(31, 0.6), back.col = FALSE,
+            main="log-likelihood for ZT binomial distribution")
+  
+}
